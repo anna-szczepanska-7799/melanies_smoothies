96197f79371c25dfea4c4c8a117ded6fbaa60df5
anna-szczepanska-7799 <myszak7799@gmail.com>
2026-09-11 15:36:31 +0200

    Update streamlit_app.py
    Connection changes from SiS to SinS.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import Python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -18,7 +17,8 @@ st.write("The name of your smoothie will be: ", order_name)
 
 # st.write('Your favourite fuit is: ', option)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAME')
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col.'FRUIT_NAME')
 
