1223802b0d39506498fa5d3f99445c10f12e467d
anna-szczepanska-7799 <myszak7799@gmail.com>
2026-09-14 11:22:45 +0200

    Update streamlit_app.py --remove df from UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAM
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 # st.stop()
 
 
