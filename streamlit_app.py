283eaa27a9dd349104003e8c98af5fa83e5eba92
anna-szczepanska-7799 <myszak7799@gmail.com>
2026-09-14 11:21:12 +0200

    Update streamlit_app.py - remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAM
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+# st.stop()
 
 
 ingredients_list = st.multiselect('Choose up to 5 ingrediients:'
